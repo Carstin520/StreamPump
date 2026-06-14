eb3fc226e001bd52eec32f14dca4f1b965aaf3c0
Carstin520 <jamescarstin520@gmail.com>
2026-06-14 22:03:28 +0800

    fix: stabilize Vercel app build

--- app/middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from "next/server";
-
-export const config = {
-  matcher: "/__streampump_noop_middleware__/:path*",
-};
-
-export function middleware() {
-  return NextResponse.next();
-}
